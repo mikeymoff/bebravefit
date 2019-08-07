5253ac7fb55a008d556f72d06cd0d16dccd6cd96
andkcho <andrewc801@gmail.com>
2019-08-06 23:53:53 -0600

    update routes

---
+++ src/components/pages/Dropin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Dropin extends Component {
+
+    render() {
+
+        return (
+            <h1 className="h3 mb-3 font-weight-normal">BeBraveFit.com</h1>
+
+      );
+    }
+}
+
+export default Dropin;
